d2c8470b50c1af87b75c6e025ce995f7221b8762
Boldween <bogdanboyko176@gmail.com>
2019-08-15 20:56:53 +0300

    First on Git Hub

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,7 +11,7 @@ public class Main {
         System.out.println("Before");
         System.out.println(story);
         System.out.println("After");
-        String s2 = story.replaceAll("вода", "водка");
+        String s2 = story.replaceAll("слово", "word");
         System.out.println(s2);
 System.out.println("First on GitHub ");
     }

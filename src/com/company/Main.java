49d1e6b293bb3189752fbabf159755de228ea944
Boldween <bogdanboyko176@gmail.com>
2019-08-15 20:28:34 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter your text");
+        Scanner in = new Scanner(System.in);
+        String story = in.nextLine();
+        System.out.println("Before");
+        System.out.println(story);
+        System.out.println("After");
+        String s2 = story.replaceAll("вода", "водка");
+        System.out.println(s2);
+
+    }
+}
+
